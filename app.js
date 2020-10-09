c0e7d6aebc91d35b7ebb3b9e133d3e492af5238a
Avinash7714 <avinash7714@gmail.com>
2020-10-09 10:56:59 +0530

    updated data

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -17,15 +18,8 @@ app.use(
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {
     listTitle: day,
@@ -34,7 +28,7 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -47,7 +41,10 @@ app.post("/", function (req, res) {
 });
 
 app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
 app.listen(3000, function () {
